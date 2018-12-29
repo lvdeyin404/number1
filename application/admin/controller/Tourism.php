71b5cb8fabfa82b531e6ba0583d85aa80363a3e3
lvdeyin404 <1141006374@qq.com>
2018-12-29 11:48:53 +0800

    1

---
+++ application/admin/controller/Tourism.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018-12-29
+ * Time: 10:53
+ */
+namespace app\admin\controller;
+class Tourism extends Common
+{
+    public function index()
+    {
+
+    }
+}
